b8730833e65a72773a243de599b5061eb41dde93
Nahuel Nicolás Giani <ngiani@ats-ar.com.ar>
2021-10-20 15:14:40 -0300

    Swagger implemented

--- src/main/java/com/temperies/movie/configuration/ModelMapperConfiguration.java
+++ src/main/java/com/temperies/movie/configuration/ModelMapperConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ModelMapperInit {
+public class ModelMapperConfiguration {
 	
 	@Bean
 	public ModelMapper modelMapper() {
